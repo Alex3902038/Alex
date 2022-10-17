db1972c228d4415877d282823cb0728bd047955b
Alex3902038 <114605041+Alex3902038@users.noreply.github.com>
2022-10-17 12:20:47 +0800

    Add files via upload
    LED点亮
    LED闪烁
    LED流水灯
    独立按键控制LED亮灭
    独立按键控制LED状态

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
-#include <at89c51RC2.h>
-void main()
+#include <REGX52.H>
+
+void Delay(unsigned int xms)		//@11.0592MHz
 {
-	P2=0xFE;//1111 1110	
+	unsigned char i, j;
+	while (xms)
+	{
+	i = 2;
+	j = 199;
+	do
+	{
+		while (--j);
+	} while (--i);
+	xms--;
+	}
 }
 
+ 
+void main()
+{
+		while(1)
+		{
+			if(P3_1==0)
+			{	
+				Delay(20);
+			while(P3_1==0)
+			{
+			
+			}
+			Delay(20);
+			
+			P2_0=~P2_0;
+			}
+		}
+}
