4e56c638bec019e7f4f31c441e6e50b25410c2b9
Alex3902038 <114605041+Alex3902038@users.noreply.github.com>
2022-10-17 12:16:22 +0800

    Add files via upload
    LED闪烁

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
-#include <at89c51RC2.h>
-void main()
+#include <REGX52.H>
+#include <INTRINS.H>
+void Delay500ms()		//@11.0592MHz
 {
-	P2=0xFE;//1111 1110	
+	unsigned char i, j, k;
+
+	_nop_();
+	i = 4;
+	j = 129;
+	k = 119;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
 }
 
+void main()
+{
+		P2=0x55;//0101 0101
+	Delay500ms();
+		P2=0xAA;
+	Delay500ms();
+}
