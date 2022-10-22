fa33d3e9fc0c735f56ff874a5f5286568e327fbc
Alex3902038 <114605041+Alex3902038@users.noreply.github.com>
2022-10-22 22:18:23 +0800

    Add files via upload

---
+++ github仓库/5-1模块化编程/Tixie.h
@@ -0,0 +1,5 @@
+#ifndef __TIXIE_H__
+#define __TIXIE_H__
+
+void Nixie (unsigned char Location,Number);
+#endif
---
+++ github仓库/5-1模块化编程/main.c
@@ -0,0 +1,13 @@
+#include <at89c51RC2.h>
+#include "Delay.h"
+#include "Tixie.h"
+int main()
+{
+	
+	while(1)
+	{
+	Nixie(1,1);
+		Nixie(2,2);
+		Nixie(3,3);
+	}
+} 
